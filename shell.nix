0329bbd4d4a5f840e9d46d45302de86a6d4cf667
jamesdorevski <james@jamesdorevski.com>
2022-09-27 19:34:18 +1000

    initial commit

---
+++ shell.nix
@@ -0,0 +1,8 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/7ad22ae49d66eb16eec6b97f9eaca9399dddbabf.tar.gz") {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    dotnet-sdk
+  ];
+}
+
