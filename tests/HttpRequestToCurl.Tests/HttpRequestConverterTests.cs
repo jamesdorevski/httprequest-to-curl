ec9b8b99ce50557677f7c2e6f6d5d2ac2c9b8efc
jamesdorevski <james@jamesdorevski.com>
2022-10-21 22:12:33 +1100

    chore: updated tests

--- tests/HttpRequestToCurl.Tests/HttpRequestConverterTests.cs
+++ tests/HttpRequestToCurl.Tests/HttpRequestConverterTests.cs
@@ -7,8 +7,10 @@ namespace HttpRequestToCurl.Tests
 	public class HttpRequestConverterTests
 	{
 		[Fact]
-		public void ConvertsGetRequestSuccessfully()
+		public void ConvertsPostRequest()
 		{
+			string expected = "curl --insecure --request POST 'https://localhost:7126/WeatherForecast' --header 'X-Hello: world' --header 'Content-Type: application/json' --data '\"Hi there!\"' ";
+			
 			var request = new HttpRequestMessage
 			{
 				RequestUri = new Uri("https://localhost:7126/WeatherForecast"),
@@ -23,7 +25,23 @@ namespace HttpRequestToCurl.Tests
 				AllowInsecureConnections = true
 			});
 
-			Assert.NotEmpty(actual);
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void ConvertsGetRequest()
+		{
+			string expected = "curl --request GET 'https://www.example.com/v1/hi' ";
+			
+			var request = new HttpRequestMessage
+			{
+				RequestUri = new Uri("https://www.example.com/v1/hi"),
+				Method = HttpMethod.Get
+			};
+
+			var actual = HttpRequestConverter.ConvertToCurl(request);
+			
+			Assert.Equal(expected, actual);
 		}
 	}
 }
